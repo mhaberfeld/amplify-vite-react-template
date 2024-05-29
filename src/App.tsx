cf6a29e1cf1a0752dac202ebbb85fd93bfad7a70
martius haberfeld <martius@midia.mobi>
2024-05-29 18:15:23 -0300

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
       {({ signOut, user }) => (
 
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
